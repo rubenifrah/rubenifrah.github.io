1935095e244a3a87d506d071270965e7b9d39325
rubenifrah <ifrah.ruben@gmail.com>
2026-03-26 09:30:51 +0000

    Deploying to gh-pages from @ rubenifrah/rubenifrah.github.io@3f52e3de8eccffffaa6747c58e5f35612f17614d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -491,6 +491,11 @@ ninja.data = [{
           description: "From-scratch implementation of Bayesian PINNs with HMC sampling for uncertainty quantification in PDE solving.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_bpinns/";
+            },},{id: "projects-reproducibility-and-critical-analysis-of-sonata",
+          title: 'Reproducibility and Critical Analysis of SONATA',
+          description: "An empirical evaluation of the geometric shortcut in 3D self-supervised learning, analyzing dense point representations of the Sonata architecture.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_SONATA/";
             },},{
         id: 'social-cv',
         title: 'CV',
